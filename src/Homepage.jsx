1344117c8636a0800cb853a4dee083406c071251
Apeksha D Ankola <116253530+apeksha-ankola@users.noreply.github.com>
2024-01-24 21:41:37 +0530

    apeksha

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import { useEffect } from "react";
 import { useGlobalContext } from "./context";
 
+import MainSection from "./Components/MainSection";
 // import Events from "./Events";
 import Members from "./Members";
 import Newsletter from "./Newsletter";
 
-import MainSection from "./Components/MainSection";
-
 const Homepage = () => {
   const { updateHomePage } = useGlobalContext();
 
-  useEffect(() => updateHomePage());
-
+  useEffect(() => {
+    // Use a callback function to avoid potential issues
+    updateHomePage();
+  }, []);
+  
   return (
     <>
       <MainSection />
