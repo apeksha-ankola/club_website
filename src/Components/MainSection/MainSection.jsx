331733ca66c5b8a1578b70e2ff646b2b18e09a6f
Apeksha D Ankola <116253530+apeksha-ankola@users.noreply.github.com>
2024-02-12 22:19:45 +0530

    Update MainSection.jsx Deploy v1.2

--- src/Components/MainSection/MainSection.jsx
+++ src/Components/MainSection/MainSection.jsx
@@ -36,7 +36,7 @@ const MainSection = () => {
             innovation, learning, and shared success!!
           </p>
           <Button className="btn explore-btn">
-            <NavLink to="/events"> Exploree </NavLink>
+            <NavLink to="/events"> Explore </NavLink>
           </Button>
         </div>
 
