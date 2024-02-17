9511e15f9998d0cdc772a90c81fa68ecedddd197
Apeksha D Ankola <116253530+apeksha-ankola@users.noreply.github.com>
2024-02-17 12:16:44 +0530

    edited cards

--- src/Components/Members/MembersData.jsx
+++ src/Components/Members/MembersData.jsx
@@ -1,63 +1,63 @@
 const membersData = [
   {
     name: "Amogh M",
-    role: "Head-Technical team",
+    role: "Technical team Head",
     imageUrl: "./images/members/amogh-bharadwaj.jpg",
     linkedinUrl: "https://www.linkedin.com/in/amogh-m-985490216/",
   },
 
   {
     name: "Anand Okade",
-    role: "Core member-Technical team",
+    role: "Committee member-Technical team",
     imageUrl: "./images/members/anand-okade.jpg",
     linkedinUrl: "https://www.linkedin.com/in/anand-okade-a9614a220/",
   },
 
   {
     name: "Ayaj Anand",
-    role: "Core member-Technical team",
+    role: "Committee member-Technical team",
     imageUrl: "./images/members/ayaj-anand.jpg",
     linkedinUrl: "https://www.linkedin.com/in/ayaj-anand-492986127/",
   },
 
   {
     name: "Amogh Athreya H",
-    role: "Core member-Marketing team",
+    role: "Committee member-Marketing team",
     imageUrl: "./images/members/amogh-athreya.jpg",
     linkedinUrl: "https://www.linkedin.com/in/amogh-athreya-454515254",
   },
   
   {
     name: "Nishitha R Lokhande",
-    role: "Core member-Technical team",
+    role: "Committee member-Technical team",
     imageUrl: "./images/members/nishitha-L.jpg",
     linkedinUrl: "https://www.linkedin.com/in/nishitha-r-lokande-094509254",
   },
 
   {
     name: "Ranjitha V",
-    role: "Core member-Organising team",
+    role: "Committee member-Organising team",
     imageUrl: "./images/members/ranjitha-s.jpg",
     linkedinUrl: "#", // Add the correct LinkedIn URL here
   },
 
   {
     name: "Sujal S",
-    role: "Core member-Organising team",
+    role: "Committee member-Organising team",
     imageUrl: "./images/members/sujal.jpg",
     linkedinUrl: "https://www.linkedin.com/in/sujal-sarode-3a5b3a25b",
   },
 
   {
     name: "Dhruva N U",
-    role: "Core member-Technical team",
+    role: "Committee member-Technical team",
     imageUrl: "./images/members/dhruva-nu.jpg",
     linkedinUrl: "https://www.linkedin.com/in/dhruva-n-u-b6788a249/",
   },
   
   {
     name: "Apeksha D Ankola",
-    role: "Core member-Technical team",
+    role: "Committee member-Technical team",
     imageUrl: "./images/members/apeksha-d.jpg",
     linkedinUrl: "https://www.linkedin.com/in/apeksha-d-ankola-aa4512254/",
   },
@@ -65,17 +65,18 @@ const membersData = [
 
   {
     name: "Aadi B Kumar",
-    role: "Head-Marketing team",
+    role: "Marketing team Head",
     imageUrl: "./images/members/aadi-kumar.jpg",
     linkedinUrl: "https://www.linkedin.com/in/aadi-b-kumar-584514254",
   },
 
   {
     name: "Amogh N Kotha",
-    role: "Head of the club",
+    role: "Club Head",
     imageUrl: "./images/members/amogh-kotha.jpg",
     linkedinUrl: "https://www.linkedin.com/in/amogh-kotha-a5617823b",
   },
+  
   
 ];
 
