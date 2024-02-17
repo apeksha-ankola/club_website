23eb644cebe3a34a08c50d3866169d9578ba5b10
Apeksha D Ankola <116253530+apeksha-ankola@users.noreply.github.com>
2024-02-17 12:19:29 +0530

    edited cards

--- src/Components/Eventss/EventsData.jsx
+++ src/Components/Eventss/EventsData.jsx
@@ -16,13 +16,13 @@ const EventsData = [
   {
     name: "BLITZ-PY-2",
     description:
-      "Our Rapid Coding Contest to showcase your coding powers as you tackle 5 AI based questions in just 30 minutes. This contest tests your ability to think quickly and to solve problems efficiently",
+      "Engage in our Rapid Coding Event, solving 5 AI based questions in just 30 minutes. Test your quick thinking and problem-solving skills!",
     imageUrl: "./images/poster2.jpg",
   },
   {
     name: "WINNERS OF S-1, EP-2",
     description:
-      "Congratulations to all !!! Here is the list of winners of BLITZ-PY-2.First place- YASH VYAS, Second place- PRINCE MISHRA, Third place- NIKHIL CHOUDARY",
+      "Congratulations to all !! Here is the list of winners of BLITZ-PY-2.First place- YASH VYAS, Second place- PRINCE MISHRA, Third place- NIKHIL CHOUDARY",
     imageUrl: "./images/winnersposter.png"
   },
 
